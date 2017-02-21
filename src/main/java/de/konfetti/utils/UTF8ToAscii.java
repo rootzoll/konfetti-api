5291bc829db4ab9b8627bb235ce00ad0d27044d4
rotzoll <christian@geektank.de>
2017-02-21 20:43:11 +0100

    try fix

--- src/main/java/de/konfetti/utils/UTF8ToAscii.java
+++ src/main/java/de/konfetti/utils/UTF8ToAscii.java
@@ -1,35 +1,10 @@
 package de.konfetti.utils;
 
 public class UTF8ToAscii {
-	
-    private static final char[] hexChar = {
-        '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
-    };
 
     public static String unicodeEscape(String s) {
     	
-    	
     return "?utf-8?b?"+"W0tvbmZldHRpXSBIw6TDpMOkbG8gZU1haWwg"+"?=";
     	
-    //return org.apache.commons.lang3.StringEscapeUtils.escapeJava(s);
-    	
-    /*
-	StringBuilder sb = new StringBuilder();
-	for (int i = 0; i < s.length(); i++) {
-	    char c = s.charAt(i);
-	    if ((c >> 7) > 0) {
-		sb.append("\\u");
-		sb.append(hexChar[(c >> 12) & 0xF]); // append the hex character for the left-most 4-bits
-		sb.append(hexChar[(c >> 8) & 0xF]);  // hex for the second group of 4-bits from the left
-		sb.append(hexChar[(c >> 4) & 0xF]);  // hex for the third group
-		sb.append(hexChar[c & 0xF]);         // hex for the last group, e.g., the right most 4-bits
-	    }
-	    else {
-		sb.append(c);
-	    }
-	}
-	return sb.toString();
-	 */
-	
     }
 }
